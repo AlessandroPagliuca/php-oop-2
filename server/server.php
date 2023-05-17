051bbfaa5cc1331720ecf8b8577844893faf3b7a
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-17 14:37:12 +0200

    server with array products

---
+++ server/server.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * products array
+ */
+$products = [
+    new Card(1, "image1.jpg", "Cibo per cani", 10.99, "Cani", "Cibo", "icona_cane.png"),
+    new Card(2, "image2.jpg", "Gioco per gatti", 5.99, "Gatti", "Gioco", "icona_gatto.png"),
+];
