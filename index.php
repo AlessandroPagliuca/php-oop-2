103983f094db7cbcfc169d6cbc3339e34f65aa01
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-17 15:15:36 +0200

    Added icon category

--- index.php
+++ index.php
@@ -19,6 +19,9 @@ include __DIR__ . '/server/server.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./style/style.css">
     <title>php-oop-2</title>
 </head>
@@ -35,7 +38,7 @@ include __DIR__ . '/server/server.php';
                     <p>
                         <?php echo $product->getPrice(); ?> €
                     </p>
-                    <img src="<?php echo $product->getIconCategory(); ?>" alt="icon">
+                    <i class="<?php echo $product->getIconCategory(); ?>"></i>
                     <p>
                         <?php echo $product->getType(); ?>
                     </p>
--- server/server.php
+++ server/server.php
@@ -3,6 +3,6 @@
  * products array
  */
 $products = [
-    new Card(1, "image1.jpg", "Cibo per cani", 10.99, "Cani", "Cibo", "icona_cane.png"),
-    new Card(2, "image2.jpg", "Gioco per gatti", 5.99, "Gatti", "Gioco", "icona_gatto.png"),
+    new Card(1, "image1.jpg", "Cibo per cani", 10.99, "Cani", "Cibo", "fa-solid fa-dog"),
+    new Card(2, "image2.jpg", "Gioco per gatti", 5.99, "Gatti", "Gioco", "fa-solid fa-cat"),
 ];
