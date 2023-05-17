cdc7cb39f641b7c4f75964d6f1b032dc6125ff39
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-05-17 16:09:41 +0200

    fix id card

--- server/server.php
+++ server/server.php
@@ -5,6 +5,6 @@
 $products = [
     new Card(1, "dog-test.webp", "Cibo per cani", 10.99, "Cani", "Cibo", "fa-solid fa-dog"),
     new Card(2, "gatti-test.jpg", "Gioco per gatti", 5.99, "Gatti", "Gioco", "fa-solid fa-cat"),
-    new Card(2, "gatti-test.jpg", "Cuccia per gatti", 9.99, "Gatti", "Cuccia", "fa-solid fa-cat"),
+    new Card(3, "gatti-test.jpg", "Cuccia per gatti", 9.99, "Gatti", "Cuccia", "fa-solid fa-cat"),
 
 ];
